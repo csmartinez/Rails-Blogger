48c018c6f3f01bcfcd60b1b3f9b17fae0a549dc6
Carli <martinezcarli@gmail.com>
2015-03-20 12:18:03 -0700

    add ability to diplay posts manually entered into database

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe Post do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it { should validate_presence_of :author }
+end
