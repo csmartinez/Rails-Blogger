761ea9259fd23f73cd9f0d8affe0e08391fb4b58
Carli <martinezcarli@gmail.com>
2015-03-20 12:51:28 -0700

    Add new post page and create method in posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     render :show
   end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to  posts_path
+    else
+      render :new
+    end
+  end
 end
